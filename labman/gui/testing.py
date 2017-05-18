3c1515f6c9416adc8131cdd3045aeb24ebee187c
Jose Navas <josenavasmolina@gmail.com>
2017-05-18 13:56:05 -0700

    Adding initial websever boilerplate code

---
+++ labman/gui/testing.py
@@ -0,0 +1,62 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2017-, labman development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from urllib.parse import urlencode
+
+from tornado.testing import AsyncHTTPTestCase
+
+from labman.gui.webserver import Application
+
+
+class TestHandlerBase(AsyncHTTPTestCase):
+    app = Application()
+
+    def get_app(self):
+        self.app.settings['debug'] = False
+        return self.app
+
+    # helpers from http://www.peterbe.com/plog/tricks-asynchttpclient-tornado
+    def get(self, url, data=None, headers=None, doseq=True):
+        if data is not None:
+            if isinstance(data, dict):
+                data = urlencode(data, doseq=doseq)
+            if '?' in url:
+                url += '&%s' % data
+            else:
+                url += '?%s' % data
+        return self._fetch(url, 'GET', headers=headers)
+
+    def post(self, url, data, headers=None, doseq=True):
+        if data is not None:
+            if isinstance(data, dict):
+                data = urlencode(data, doseq=doseq)
+        return self._fetch(url, 'POST', data, headers)
+
+    def patch(self, url, data, headers=None, doseq=True):
+        if isinstance(data, dict):
+            data = urlencode(data, doseq=doseq)
+        if '?' in url:
+            url += '&%s' % data
+        else:
+            url += '?%s' % data
+        return self._fetch(url, 'PATCH', data=data, headers=headers)
+
+    def delete(self, url, data=None, headers=None, doseq=True):
+        if data is not None:
+            if isinstance(data, dict):
+                data = urlencode(data, doseq=doseq)
+            if '?' in url:
+                url += '&%s' % data
+            else:
+                url += '?%s' % data
+        return self._fetch(url, 'DELETE', headers=headers)
+
+    def _fetch(self, url, method, data=None, headers=None):
+        self.http_client.fetch(self.get_url(url), self.stop, method=method,
+                               body=data, headers=headers)
+        return self.wait(timeout=15)
