dba9d767df1282e27b11c69141e8e1af1b48a727
Jose Navas <josenavasmolina@gmail.com>
2017-05-30 13:47:47 -0700

    Adding Qiita configuration to the configuration manager

--- labman/db/tests/test_configuration_manager.py
+++ labman/db/tests/test_configuration_manager.py
@@ -6,18 +6,19 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
-from unittest import TestCase, main
+from unittest import main
 from tempfile import NamedTemporaryFile
 
 from labman.db.configuration_manager import ConfigurationManager
+from labman.db.testing import LabmanTestCase
 
 
-class TestConfigurationManager(TestCase):
+class TestConfigurationManager(LabmanTestCase):
     def test_create(self):
         with NamedTemporaryFile() as tmp_f:
             ConfigurationManager.create(
                 tmp_f.name, True, 'db_host', 'db_port', 'db_name', 'db_user',
-                'db_password', 'db_admin_user', 'db_admin_password')
+                'db_password', 'db_admin_user', 'db_admin_password', False)
 
             with open(tmp_f.name) as obs_f:
                 obs = obs_f.read()
@@ -29,21 +30,71 @@ class TestConfigurationManager(TestCase):
             # when the test is run
             self.assertEqual(obs[1:], exp)
 
+    def test_create_qiita(self):
+        with NamedTemporaryFile() as tmp_f:
+            ConfigurationManager.create(
+                tmp_f.name, True, 'db_host', 'db_port', 'db_name', 'db_user',
+                'db_password', 'db_admin_user', 'db_admin_password', True,
+                'https://localhost:21174', 'client_id', 'client_secret',
+                'server_cert')
+
+            with open(tmp_f.name) as obs_f:
+                obs = obs_f.read()
+
+            obs = obs.splitlines()
+            exp = EXP_CONFIG_FILE_QIITA.splitlines()
+
+            # Removing the first line as it contains a date that is generated
+            # when the test is run
+            self.assertEqual(obs[1:], exp)
+
 
 EXP_CONFIG_FILE = """
 # ------------------------- MAIN SETTINGS ----------------------------------
 [main]
-TEST_ENVIRONMENT = True
+TEST_ENVIRONMENT=True
 
 # ----------------------- POSTGRES SETTINGS --------------------------------
 [postgres]
-USER = db_user
-PASSWORD = db_password
-ADMIN_USER = db_admin_user
-ADMIN_PASSWORD = db_admin_password
-DATABASE = db_name
-HOST = db_host
-PORT = db_port
+USER=db_user
+PASSWORD=db_password
+ADMIN_USER=db_admin_user
+ADMIN_PASSWORD=db_admin_password
+DATABASE=db_name
+HOST=db_host
+PORT=db_port
+
+# ------------------------- QIITA SETTINGS ----------------------------------
+[qiita]
+ENABLE_QIITA=False
+HOST=
+CLIENT_ID=
+CLIENT_SECRET=
+SERVER_CERT=
+"""
+
+EXP_CONFIG_FILE_QIITA = """
+# ------------------------- MAIN SETTINGS ----------------------------------
+[main]
+TEST_ENVIRONMENT=True
+
+# ----------------------- POSTGRES SETTINGS --------------------------------
+[postgres]
+USER=db_user
+PASSWORD=db_password
+ADMIN_USER=db_admin_user
+ADMIN_PASSWORD=db_admin_password
+DATABASE=db_name
+HOST=db_host
+PORT=db_port
+
+# ------------------------- QIITA SETTINGS ----------------------------------
+[qiita]
+ENABLE_QIITA=True
+HOST=https://localhost:21174
+CLIENT_ID=client_id
+CLIENT_SECRET=client_secret
+SERVER_CERT=server_cert
 """
 
 
