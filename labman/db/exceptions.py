4d80a865d20faed74a12d16600918e117ea8668e
Jose Navas <josenavasmolina@gmail.com>
2017-05-31 08:34:12 -0700

    Adding base object, exceptions and test utility functions

---
+++ labman/db/exceptions.py
@@ -0,0 +1,50 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2017-, labman development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+
+class LabmanError(Exception):
+    """Base class for all labman exceptions"""
+    pass
+
+
+class LabmanUnknownIdError(LabmanError):
+    """Exception for error when an object doesn't exist in the DB
+
+    Parameters
+    ----------
+    obj_name : str
+        The name of the object
+    obj_id : str
+        The unknown id
+    """
+    def __init__(self, obj_name, obj_id):
+        super(LabmanUnknownIdError, self).__init__()
+        self.args = "%s with ID '%s' does not exist" % (obj_name, obj_id)
+
+
+class LabmanDuplicateError(LabmanError):
+    """Exception for error when duplicates occur
+
+    Parameters
+    ----------
+    obj_name : str
+        The name of the object
+    attributes : list of (str, str)
+        The duplicated attributes
+    """
+    def __init__(self, obj_name, attributes):
+        super(LabmanDuplicateError, self).__init__()
+        attr = ', '.join(["%s = %s" % (key, val) for key, val in attributes])
+        self.args = "%s with %s already exists" % (obj_name, attr)
+
+
+class LabmanLoginError(LabmanError):
+    """Exception for error when login in"""
+    def __init__(self):
+        super(LabmanLoginError, self).__init__()
+        self.args = "Incorrect user id or password"
---
+++ labman/db/testing.py
@@ -0,0 +1,34 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2017-, labman development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from unittest import TestCase, skipIf
+
+from labman.db.environment_manager import (
+    is_test_environment, destroy_environment, make_environment)
+from labman.db.settings import labman_settings
+
+
+class LabmanTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        if not is_test_environment():
+            raise RuntimeError(
+                "Working on a production environment. Not executing tests to "
+                "protect the production database.")
+        # Reset the test database to enforce test independence at the class
+        # level
+        destroy_environment(False)
+        make_environment(False)
+
+
+def qiita_skip_test():
+    """Decorator to use to mark tests that should be skipped when labman
+    is not configured to run with Qiita
+    """
+    return skipIf(not labman_settings.qiita_enabled,
+                  "Current labman installation is not Qiita enabled")
