05462084811a790653a161264a0a8fb7ad56147a
Jose Navas <josenavasmolina@gmail.com>
2017-06-30 09:35:45 -0700

    Removing unused code and addapt code to be using the Qiita database

--- labman/db/testing.py
+++ labman/db/testing.py
@@ -8,21 +8,32 @@
 
 from unittest import TestCase, skipIf
 
-from labman.db.environment_manager import (
-    is_test_environment, reset_test_database)
+from qiita_client import QiitaClient
+
 from labman.db.settings import labman_settings
+from labman.db.sql_connection import TRN
 
 
 class LabmanTestCase(TestCase):
     @classmethod
     def setUpClass(cls):
-        if not is_test_environment():
-            raise RuntimeError(
-                "Working on a production environment. Not executing tests to "
-                "protect the production database.")
+        with TRN:
+            TRN.add("SELECT test FROM settings")
+            if not TRN.execute_fetchlast():
+                raise RuntimeError(
+                    "Working on a production environment. Not executing "
+                    "tests to protect the production database.")
         # Reset the test database to enforce test independence at the class
-        # level
-        reset_test_database()
+        # level. The client id and client secret are hardcoded because these
+        # should only be used in the test environment. If this fails, it would
+        # mean that the Qiita installation is not a test installation
+        client_id = '19ndkO3oMKsoChjVVWluF7QkxHRfYhTKSFbAVt8IhK7gZgDaO4'
+        client_secret = ('J7FfQ7CQdOxuKhQAf1eoGgBAE81Ns8Gu3EKaWFm3IO2JKh'
+                         'AmmCWZuabe0O5Mp28s1')
+        qclient = QiitaClient(
+            "https://localhost:21174", client_id, client_secret,
+            server_cert=labman_settings.qiita_server_cert)
+        qclient.post("/apitest/reset/")
 
 
 def qiita_skip_test():
