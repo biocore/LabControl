a1530a356da57654e4394fcd5b7a3f321ad6a220
Jose Navas <josenavasmolina@gmail.com>
2017-08-25 07:37:23 -0700

    Moving to Qiita schema

--- labman/db/support_files/db_patch_manual.sql
+++ labman/db/support_files/db_patch_manual.sql
@@ -1,1 +1,1 @@
-ALTER TABLE labman.gdna_well ADD CONSTRAINT fk_gdna_well_gdna_content_id FOREIGN KEY ( content_id ) REFERENCES qiita.study_sample( sample_id );
+ALTER TABLE qiita.gdna_well ADD CONSTRAINT fk_gdna_well_gdna_content_id FOREIGN KEY ( content_id ) REFERENCES qiita.study_sample( sample_id );
