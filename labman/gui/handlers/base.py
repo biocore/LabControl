2c32e686968d466a14b7b66dc1fc1e2cc2fd4a13
Jose Navas <josenavasmolina@gmail.com>
2017-08-30 10:41:05 -0700

    Improving error page

--- labman/gui/handlers/base.py
+++ labman/gui/handlers/base.py
@@ -41,12 +41,17 @@ class BaseHandler(RequestHandler):
             exc_info = kwargs["exc_info"]
             trace_info = ''.join(
                 ["%s<br />" % line for line in format_exception(*exc_info)])
-            request_info = ''.join(
-                ["<strong>%s</strong>: %s<br/>" % (k, self.request.__dict__[k])
-                 for k in self.request.__dict__.keys()])
             error = exc_info[1]
-            self.render('error.html', error=error, trace_info=trace_info,
-                        request_info=request_info)
+        else:
+            error = "No exc_info generated. Status code: %s" % status_code
+            trace_info = "Missing trace info"
+
+        request_info = ''.join(
+            ["<strong>%s</strong>: %s<br/>" % (k, self.request.__dict__[k])
+             for k in self.request.__dict__.keys()])
+
+        self.render('error.html', error=error, trace_info=trace_info,
+                    request_info=request_info)
 
     def head(self):
         """Adds proper response for head requests"""
