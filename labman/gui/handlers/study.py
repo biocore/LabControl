6790b376b831545f380e2ce98f9653c0cf61bff4
Jose Navas <josenavasmolina@gmail.com>
2017-09-28 09:49:53 -0700

    Adding endpoint to search samples from a study

--- labman/gui/handlers/study.py
+++ labman/gui/handlers/study.py
@@ -7,6 +7,7 @@
 # ----------------------------------------------------------------------------
 
 from tornado.web import authenticated
+from tornado.escape import json_encode
 
 from labman.gui.handlers.base import BaseHandler
 from labman.db.study import Study
@@ -32,6 +33,19 @@ class StudyHandler(BaseHandler):
             self.write({'study_id': study.id,
                         'study_title': study.title,
                         'total_samples': study.num_samples})
+        except LabmanUnknownIdError:
+            self.set_status(404)
+        self.finish()
+
+
+class StudySearchSamplesHandler(BaseHandler):
+    @authenticated
+    def get(self, study_id):
+        try:
+            study = Study(int(study_id))
+            term = self.get_argument('term')
+            res = [{'label': s, 'value': s} for s in study.samples(term)]
+            self.write(json_encode(res))
         except LabmanUnknownIdError:
             self.set_status(404)
         self.finish()
--- labman/gui/test/test_study.py
+++ labman/gui/test/test_study.py
@@ -13,8 +13,8 @@ from tornado.escape import json_decode
 from labman.gui.testing import TestHandlerBase
 
 
-class TestStudyListHandler(TestHandlerBase):
-    def test_get(self):
+class TestStudyHandlers(TestHandlerBase):
+    def test_get_study_list_handler(self):
         response = self.get('/study_list')
         self.assertEqual(response.code, 200)
         obs = json_decode(response.body)
@@ -23,9 +23,7 @@ class TestStudyListHandler(TestHandlerBase):
              'Cannabis Soils', 'test@foo.bar', 27]]}
         self.assertEqual(obs, exp)
 
-
-class TestStudyHandler(TestHandlerBase):
-    def test_get(self):
+    def test_get_study_handler(self):
         response = self.get('/study/1/')
         self.assertEqual(response.code, 200)
         obs = json_decode(response.body)
@@ -35,9 +33,42 @@ class TestStudyHandler(TestHandlerBase):
                'total_samples': 27}
         self.assertEqual(obs, exp)
 
-        # Test non-existend study
+        # Test non-existent study
         response = self.get('/study/400/')
         self.assertEqual(response.code, 404)
+
+    def test_get_study_search_samples_handler(self):
+        response = self.get('/study/1/sample_search?term=SKB')
+        self.assertEqual(response.code, 200)
+        obs = json_decode(response.body)
+        exp = [{'label': '1.SKB1.640202', 'value': '1.SKB1.640202'},
+               {'label': '1.SKB2.640194', 'value': '1.SKB2.640194'},
+               {'label': '1.SKB3.640195', 'value': '1.SKB3.640195'},
+               {'label': '1.SKB4.640189', 'value': '1.SKB4.640189'},
+               {'label': '1.SKB5.640181', 'value': '1.SKB5.640181'},
+               {'label': '1.SKB6.640176', 'value': '1.SKB6.640176'},
+               {'label': '1.SKB7.640196', 'value': '1.SKB7.640196'},
+               {'label': '1.SKB8.640193', 'value': '1.SKB8.640193'},
+               {'label': '1.SKB9.640200', 'value': '1.SKB9.640200'}]
+        self.assertEqual(obs, exp)
+
+        response = self.get('/study/1/sample_search?term=SKB1')
+        self.assertEqual(response.code, 200)
+        obs = json_decode(response.body)
+        exp = [{'label': '1.SKB1.640202', 'value': '1.SKB1.640202'}]
+        self.assertEqual(obs, exp)
+
+        response = self.get('/study/1/sample_search?term=1.64')
+        self.assertEqual(response.code, 200)
+        obs = json_decode(response.body)
+        exp = [{'label': '1.SKB1.640202', 'value': '1.SKB1.640202'},
+               {'label': '1.SKD1.640179', 'value': '1.SKD1.640179'},
+               {'label': '1.SKM1.640183', 'value': '1.SKM1.640183'}]
+        self.assertEqual(obs, exp)
+
+        # test non-existent study
+        response = self.get('/study/400/sample_search')
+
 
 if __name__ == '__main__':
     main()
