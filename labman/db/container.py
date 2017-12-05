7b6de556cd013cd6bf28bed96cc525ca81221210
Jose Navas <josenavasmolina@gmail.com>
2017-12-04 18:02:55 -0800

    Adding container tests

--- labman/db/container.py
+++ labman/db/container.py
@@ -6,13 +6,13 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
+from . import base
+from . import sql_connection
+from . import plate
+from . import process
 
-from labman.db.base import LabmanObject
-from labman.db.sql_connection import TRN
-from labman.db.plate import Plate
 
-
-class _Container(LabmanObject):
+class _Container(base.LabmanObject):
     """Container object
 
     Attributes
@@ -22,6 +22,25 @@ class _Container(LabmanObject):
     notes
     latest_process
     """
+    @classmethod
+    def _common_creation_steps(cls, process, remaining_volume):
+        with sql_connection.TRN as TRN:
+            sql = """SELECT container_type_id
+                     FROM qiita.container_type
+                     WHERE description = %s"""
+            TRN.add(sql, [cls._container_type])
+            ct_id = TRN.execute_fetchlast()
+
+            sql = """INSERT INTO qiita.container
+                        (container_type_id, latest_upstream_process_id,
+                         remaining_volume)
+                     VALUES (%s, %s, %s)
+                     RETURNING container_id"""
+            TRN.add(sql, [ct_id, process.process_id, remaining_volume])
+            container_id = TRN.execute_fetchlast()
+
+        return container_id
+
     def _get_container_attr(self, attr):
         """Returns the value of the given container attribute
 
@@ -35,10 +54,10 @@ class _Container(LabmanObject):
         Object
             The attribute
         """
-        with TRN:
+        with sql_connection.TRN as TRN:
             sql = """SELECT {}
                      FROM qiita.container
-                        JOIN {} USING container_id
+                        JOIN {} USING (container_id)
                      WHERE {} = %s""".format(attr, self._table,
                                              self._id_column)
             TRN.add(sql, [self.id])
@@ -57,9 +76,12 @@ class _Container(LabmanObject):
     @property
     def latest_process(self):
         """The latest process applied to the container"""
-        # TODO: Once the process object exists, return the process object
-        # instead of the id
-        return self._get_container_attr('latest_upstream_process_id')
+        return process.Process.factory(
+            self._get_container_attr('latest_upstream_process_id'))
+
+    @property
+    def container_id(self):
+        return self._get_container_attr('container_id')
 
 
 class Tube(_Container):
@@ -104,11 +126,43 @@ class Well(_Container):
     """
     _table = "qiita.well"
     _id_column = "well_id"
+    _container_type = 'well'
+
+    @classmethod
+    def create(cls, plate, process, volume, row, col):
+        """Creates a new well
+
+        Parameters
+        ----------
+        plate: labman.db.Plate
+            The plate to which this well belongs to
+        process: labman.db.Process
+            The process that generated this well
+        volume : float
+            The initial volume of the well
+        row : int
+            The row number of the well
+        col : int
+            The column number of the well
+
+        Returns
+        -------
+        labman.db.Well
+        """
+        with sql_connection.TRN as TRN:
+            container_id = cls._common_creation_steps(process, volume)
+            sql = """INSERT INTO qiita.well
+                        (container_id, plate_id, row_num, col_num)
+                     VALUES (%s, %s, %s, %s)
+                     RETURNING well_id"""
+            TRN.add(sql, [container_id, plate.id, row, col])
+            well_id = TRN.execute_fetchlast()
+        return cls(well_id)
 
     @property
     def plate(self):
         """The plate the well belongs to"""
-        return Plate(self._get_attr('plate_id'))
+        return plate.Plate(self._get_attr('plate_id'))
 
     @property
     def row(self):
--- labman/db/tests/test_container.py
+++ labman/db/tests/test_container.py
@@ -6,5 +6,40 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
-# TODO: Creating this classes is a result of more complex interactions
-# write the test once we can create those objects
+from unittest import main
+
+from labman.db.testing import LabmanTestCase
+from labman.db.container import Well, Tube
+from labman.db.plate import Plate
+from labman.db.process import SamplePlatingProcess, PoolingProcess
+
+
+class TestTube(LabmanTestCase):
+    # The creation of a tube is always linked to a Process, we are going to
+    # test the creation of a tube on those processes rather than here
+    def test_properties(self):
+        tester = Tube(4)
+        self.assertEqual(tester.external_id, 'Test Pool from Plate 1')
+        self.assertFalse(tester.discarded)
+        self.assertEqual(tester.remaining_volume, 96)
+        self.assertIsNone(tester.notes)
+        self.assertEqual(tester.latest_process, PoolingProcess(1))
+        self.assertEqual(tester.container_id, 1540)
+
+
+class TestWell(LabmanTestCase):
+    # The creation of a well is always linked to a Process, we are going to
+    # test the creation of a well on those processes rather than here
+    def test_properties(self):
+        tester = Well(1537)
+        self.assertEqual(tester.plate, Plate(17))
+        self.assertEqual(tester.row, 1)
+        self.assertEqual(tester.column, 1)
+        self.assertEqual(tester.remaining_volume, 10)
+        self.assertIsNone(tester.notes)
+        self.assertEqual(tester.latest_process, SamplePlatingProcess(4))
+        self.assertEqual(tester.container_id, 1542)
+
+
+if __name__ == '__main__':
+    main()
