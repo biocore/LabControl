5e77ebc7e6f82c489a435d0ba72025ce1409e081
Jose Navas <josenavasmolina@gmail.com>
2018-01-15 11:10:06 -0800

    Fixing all db tests

--- labman/db/tests/test_container.py
+++ labman/db/tests/test_container.py
@@ -17,29 +17,29 @@ from labman.db.composition import PoolComposition, SampleComposition
 
 class TestContainer(LabmanTestCase):
     def test_factory(self):
-        self.assertEqual(Container.factory(1540), Tube(4))
-        self.assertEqual(Container.factory(1829), Well(1824))
+        self.assertEqual(Container.factory(3076), Tube(4))
+        self.assertEqual(Container.factory(1824), Well(1824))
 
 
 class TestTube(LabmanTestCase):
     # The creation of a tube is always linked to a Process, we are going to
     # test the creation of a tube on those processes rather than here
     def test_properties(self):
-        tester = Tube(4)
+        tester = Tube(6)
         self.assertEqual(tester.external_id, 'Test Pool from Plate 1')
         self.assertFalse(tester.discarded)
         self.assertEqual(tester.remaining_volume, 96)
         self.assertIsNone(tester.notes)
         self.assertEqual(tester.latest_process, PoolingProcess(1))
-        self.assertEqual(tester.container_id, 1540)
+        self.assertEqual(tester.container_id, 3078)
         self.assertEqual(tester.composition, PoolComposition(1))
 
     def test_discarded(self):
-        tester = Tube(5)
+        tester = Tube(7)
         self.assertFalse(tester.discarded)
         tester.discard()
         self.assertTrue(tester.discarded)
-        regex = "Can't discard tube 5: it's already discarded."
+        regex = "Can't discard tube 7: it's already discarded."
         self.assertRaisesRegex(ValueError, regex, tester.discard)
 
 
@@ -47,14 +47,14 @@ class TestWell(LabmanTestCase):
     # The creation of a well is always linked to a Process, we are going to
     # test the creation of a well on those processes rather than here
     def test_properties(self):
-        tester = Well(1537)
-        self.assertEqual(tester.plate, Plate(17))
+        tester = Well(3073)
+        self.assertEqual(tester.plate, Plate(21))
         self.assertEqual(tester.row, 1)
         self.assertEqual(tester.column, 1)
         self.assertEqual(tester.remaining_volume, 10)
         self.assertIsNone(tester.notes)
-        self.assertEqual(tester.latest_process, SamplePlatingProcess(6))
-        self.assertEqual(tester.container_id, 1542)
+        self.assertEqual(tester.latest_process, SamplePlatingProcess(10))
+        self.assertEqual(tester.container_id, 3081)
         self.assertEqual(tester.composition, SampleComposition(1))
 
     def test_well_id(self):
