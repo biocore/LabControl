ebe30f9a202452a9249d9efc13779c4022654cac
Daniel McDonald <mcdonadt@colorado.edu>
2018-01-23 16:30:11 -0800

    Allow prepopulation

--- labman/gui/handlers/process_handlers/gdna_compression_process.py
+++ labman/gui/handlers/process_handlers/gdna_compression_process.py
@@ -12,13 +12,33 @@ from tornado.escape import json_decode
 from labman.gui.handlers.base import BaseHandler
 from labman.db.process import GDNAPlateCompressionProcess
 from labman.db.plate import Plate
+from labman.db.exceptions import LabmanUnknownIdError
 
 
 class GDNAPlateCompressionProcessHandler(BaseHandler):
     @authenticated
     def get(self):
         plate_ids = self.get_arguments('plate_id')
-        self.render('compression.html', plate_ids=plate_ids)
+        process_id = self.get_argument('process_id', None)
+
+        if process_id is not None:
+            # bail if a process id and a plate id is specified
+            if plate_ids:
+                self.set_status(400)
+                self.finish()
+                return
+
+            try:
+                process = GDNAPlateCompressionProcess(process_id)
+            except LabmanUnknownIdError:
+                self.set_status(404)
+                self.finish()
+                return
+
+            plate_ids = [plate.id for plate in process.plates]
+
+        self.render('compression.html', plate_ids=plate_ids,
+                    process_id=process_id)
 
     @authenticated
     def post(self):
