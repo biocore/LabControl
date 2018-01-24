fed24b285ce576976dbb8c4e11a56fcf502df9e7
qiyunzhu <qiyunzhu@gmail.com>
2018-01-24 11:28:08 -0800

    fixed flake8 error

--- labman/gui/handlers/process_handlers/equipment_creation_process.py
+++ labman/gui/handlers/process_handlers/equipment_creation_process.py
@@ -6,8 +6,6 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
-from datetime import date
-
 from tornado.web import authenticated
 
 from labman.gui.handlers.base import BaseHandler
