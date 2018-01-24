52f4fe879c0dda2e5dbb27d1ec38c425974b0321
root <root@DESKTOP-DC3MPC5.localdomain>
2018-01-24 10:02:55 -0800

    Moved js to plate.js

--- labman/gui/static/js/plate.js
+++ labman/gui/static/js/plate.js
@@ -111,3 +111,114 @@ function change_plate_configuration() {
     pv = new PlateViewer('plate-map-div', undefined, undefined, $opt.attr('pm-data-rows'), $opt.attr('pm-data-cols'));
   }
 }
+
+/**
+ *
+ * Add html per plate
+ *
+ **/
+ function add_plate_html(plateId){
+
+   html = $("<div id=plate-"+plateId+"></div>");
+   html.append($("<label></label>").append("<h3>Plate '<span id='plateName' class='plateName-"+plateId+"'>New Plate</span>'</h3>"));
+   // Add buttons next to the plate name
+   html.append("<button class='btn btn-default' data-toggle='modal'"+
+   " data-target='#updatePlateName'>"+
+   "<span class='glyphicon glyphicon-edit'></span> Change name</button>")
+       .append("<a class='btn btn-success' href='/'>Save</a>");
+
+   // Plate configuration div
+   formGroup = $("<div class='form-group'></div>");
+   formGroup.append("<label class='control-label'><h4>Plate configuration:</h4></label>");
+   $('#plate-conf-select').clone().appendTo(formGroup);
+   html.append(formGroup);
+
+   div = $("<div></div>");
+   div.append($("<label></label>").append("<h4>Studies being plated</h4>"));
+   div.append("<button class='btn btn-success' data-toggle='modal' data-target='#addStudyModal'><span class='glyphicon glyphicon-plus'></span> Add study</button>");
+   div.append("<div id='study-list'></div>");
+   html.append(div);
+
+   // Plate map div
+   html.append("<h4>Plate Map</h4>");
+   html.append("<div id='plate-map-div' style='width:100%;height:250px'></div>");
+
+   html.append("<h4>Well comments</h4>");
+   html.append("<div id='well-plate-comments' class='comment-box'></div>");
+
+
+   $('.form-group').remove()
+   $('#work_area').append(html);
+ }
+
+/**
+ *
+ * Create layout per plateViewer
+ *
+ **/
+function add_plate_html_functions(processId, plateId){
+  var parent = $('#plate-'+plateId);
+  // Set the focus on the text input when the modal to change the plate name
+  // is shown. Also reset any other change that we may have done to the
+  // modal
+  parent.find('#updatePlateName').on('shown.bs.modal', function () {
+    parent.find('#newNameInput').focus().prop('disabled', false).val('');
+    parent.find('#updateNameBtn').prop('disabled', true).find('span').removeClass('glyphicon glyphicon-refresh gly-spin');
+    parent.find('#newNameDiv').removeClass('has-success').addClass('has-error').find('span').removeClass('glyphicon-ok').addClass('glyphicon-remove');
+    var pId = parent.find('#plateName').prop('pm-data-plate-id');
+    if (pId !== undefined) {
+      // The user is modifying an existing plate - do not force the user
+      // to introduce a plate name to close the modal
+      parent.find('#updatePlateNameCloseBtn').prop('hidden', false);
+      parent.find('#updatePlateName').data('bs.modal').options.keyboard = true;
+      parent.find('#updatePlateName').data('bs.modal').options.backdrop = true;
+      // Remove the cancel button from the modal
+      parent.find('#cancelUpdateNameBtn').remove();
+    }
+  });
+    // Set the validation on the plate name
+  parent.find('#newNameInput').keyup(function(e) {
+    onKeyUpPlateName(e, 'newNameInput', function() {
+      parent.find('#updateNameBtn').prop('disabled', parent.find('#newNameDiv').hasClass('has-error'));
+    }, change_plate_name);
+  });
+
+
+  // Set the study search table
+  var table = parent.find('#searchStudyTable').DataTable(
+    {'ajax': '/study_list',
+     'columnDefs': [{'targets': -1,
+                     'data': null,
+                     'render': function(data, type, row, meta) {
+                        var studyId = data[0];
+                        return "<button id='addBtnStudy" + studyId + "' class='btn btn-success btn-circle-small'><span class='glyphicon glyphicon-plus'></span></button>";
+                      }}]
+    });
+  // Add the function to the buttons that add the study to the plate
+  parent.find('#searchStudyTable tbody').on('click', 'button', function() {
+    add_study(table.row( $(this).parents('tr') ).data()[0]);
+  });
+
+  if (plateId === undefined) {
+
+    // The user is creating a new plate. Force the user to introduce a new
+    // plate name
+    parent.find('#updatePlateName').modal('show');
+  } else {
+    // Set the plateId in a prop so we can recover it whenever we want
+    parent.find('.plateName-'+plateId).attr('pm-data-plate-id', plateId);
+    // Disable the plate config select
+    parent.find('.plate-conf-select').attr('disabled', true);
+    // Update the plate name in the interface
+    $.get('/plate/' + plateId + '/', function(data) {
+      parent.find('#plateName').html(data['plate_name']);
+      parent.find('#plate-conf-select').val(data['plate_configuration'][0]);
+    });
+    // Load the plate map
+    var pv = new PlateViewer('plate-map-div', plateId, processId);
+
+  }
+
+  // Add the change callback to the plate configuration select
+  $('#plate-conf-select').on('change', change_plate_configuration);
+}
