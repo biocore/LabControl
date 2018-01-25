5719441721757961aaa2a77929eda75a2cf02493
Jose Navas <josenavasmolina@gmail.com>
2018-01-24 21:49:28 -0800

    Fixing gdna interface

--- labman/gui/handlers/process_handlers/gdna_compression_process.py
+++ labman/gui/handlers/process_handlers/gdna_compression_process.py
@@ -6,28 +6,47 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
-from tornado.web import authenticated
+from tornado.web import authenticated, HTTPError
 from tornado.escape import json_decode
 
 from labman.gui.handlers.base import BaseHandler
 from labman.db.process import GDNAPlateCompressionProcess
 from labman.db.plate import Plate
+from labman.db.equipment import Equipment
+from labman.db.exceptions import LabmanUnknownIdError
 
 
 class GDNAPlateCompressionProcessHandler(BaseHandler):
     @authenticated
     def get(self):
         plate_ids = self.get_arguments('plate_id')
-        self.render('compression.html', plate_ids=plate_ids)
+        process_id = self.get_argument('process_id', None)
+        plate_name = None
+        echo = None
+        gdna_plates = []
+        if process_id is not None:
+            try:
+                process = GDNAPlateCompressionProcess(process_id)
+            except LabmanUnknownIdError:
+                raise HTTPError(404, reason="Compression process %s doesn't "
+                                            "exist" % process_id)
+            plate_name = process.plates[0].external_id
+            echo = process.robot.id
+            gdna_plates = [p.id for p in process.gdna_plates]
+        echos = Equipment.list_equipment('echo')
+        self.render('compression.html', plate_ids=plate_ids, echos=echos,
+                    plate_name=plate_name, echo=echo, gdna_plates=gdna_plates,
+                    process_id=process_id)
 
     @authenticated
     def post(self):
         plates = self.get_argument('plates')
         plate_ext_id = self.get_argument('plate_ext_id')
+        echo = self.get_argument('echo')
 
         plates = [Plate(pid) for pid in json_decode(plates)]
 
         process = GDNAPlateCompressionProcess.create(
-            self.current_user, plates, plate_ext_id)
+            self.current_user, plates, plate_ext_id, Equipment(echo))
 
         self.write({'process': process.id})
