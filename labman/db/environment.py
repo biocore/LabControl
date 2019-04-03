313aad4d5665bed18541de8be8be3d2226ee1e77
Daniel McDonald <danielmcdonald@ucsd.edu>
2019-04-03 16:30:47 -0700

    Missed an import

--- labman/db/environment.py
+++ labman/db/environment.py
@@ -1,5 +1,13 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2017-, labman development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
 from os.path import join, dirname, abspath, basename, splitext, exists
 from glob import glob
+from functools import partial
 
 from natsort import natsorted
 
