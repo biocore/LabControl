83f74e51004432deab01aefbfc45ce347435667f
Charles Cowart <ccowart@BigMac.local>
2019-04-29 13:37:22 -0700

    Arbitrary change to restart Travis Integration

--- labcontrol/db/process.py
+++ labcontrol/db/process.py
@@ -3611,7 +3611,7 @@ class SequencingProcess(Process):
         a dict of the Study and the prep
         """
 
-        # currently, this implementation is a work in progress
+        # TODO: currently, this implementation is a work in progress
         raise ValueError("Prep file generation is not implemented for {} "
                          "assays.".format(self.assay))
 
