5b8d2d256192f7629b187d8c37ea92f40b142f41
Amanda Birmingham <abirmingham@ucsd.edu>
2019-05-09 16:09:39 -0700

    Corrected undesired shadowing of built in "input" variable

--- labcontrol/db/tests/test_process.py
+++ labcontrol/db/tests/test_process.py
@@ -1544,9 +1544,9 @@ class TestSequencingProcess(LabControlTestCase):
         self.assertEqual(SequencingProcess._bcl_scrub_name('test_1'), 'test_1')
 
     def test__folder_scrub_name(self):
-        input = "Ogden  Bogden-Meade*,_Pat O'Brien_1"
+        input_str = "Ogden  Bogden-Meade*,_Pat O'Brien_1"
         exp = "Ogden_Bogden-Meade-_Pat_O-Brien_1"
-        obs = SequencingProcess._folder_scrub_name(input)
+        obs = SequencingProcess._folder_scrub_name(input_str)
         self.assertEqual(obs, exp)
 
     def test_reverse_complement(self):
