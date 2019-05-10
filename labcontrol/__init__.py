0336b8d89378f940bdc92790e62eb17778a1c488
Daniel McDonald <danielmcdonald@ucsd.edu>
2019-05-09 17:31:33 -0700

    BLD: adding versioneer

--- labcontrol/__init__.py
+++ labcontrol/__init__.py
@@ -5,3 +5,7 @@
 #
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
