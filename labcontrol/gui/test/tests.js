c199ec347bc784a98309557891be99520e6c07b5
Charles Cowart <ccowart@BigMac.local>
2019-05-28 17:19:16 -0700

    Arbitrary Retest
    It appears that unittest failure may have been arbitrary and due to
    timeout.

--- labcontrol/gui/test/tests.js
+++ labcontrol/gui/test/tests.js
@@ -1,3 +1,7 @@
 QUnit.test( "hello test", function( assert ) {
   assert.ok( 1 == "1", "Passed!" );
 });
+
+QUnit.test( "hello test II", function( assert ) {
+  assert.ok( 1 == "1", "Passed!" );
+});
