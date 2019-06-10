99c6155254cdf3c0fe42aad65b6ef2cad4a24a6d
Charles Cowart <ccowart@BigMac.local>
2019-06-10 13:18:28 -0700

    Rename labman to labcontrol
    Rename remaining instances of 'labman' schema to 'labcontrol'. Since the
    patching system itself depends on a fixed schema name when connecting to
    the database, this rename can't easily be done within the patching
    system itself. Existing labcontrol schemas can be renamed manually using
    'ALTER SCHEMA labman RENAME TO labcontrol;'.

--- labcontrol/db/support_files/patches/1.sql
+++ labcontrol/db/support_files/patches/1.sql
@@ -1,8 +1,8 @@
 -- April 2, 2019
 -- Implicitly track the date and time of plate creation
-ALTER TABLE labman.plate ADD COLUMN creation_timestamp timestamp NOT NULL default now();
+ALTER TABLE labcontrol.plate ADD COLUMN creation_timestamp timestamp NOT NULL default now();
 
 -- May 9, 2019
 -- Add constraints to existing columns for wells.
-ALTER TABLE labman.well ADD CONSTRAINT rowchk CHECK (row_num > 0);
-ALTER TABLE labman.well ADD CONSTRAINT colchk CHECK (col_num > 0);
+ALTER TABLE labcontrol.well ADD CONSTRAINT rowchk CHECK (row_num > 0);
+ALTER TABLE labcontrol.well ADD CONSTRAINT colchk CHECK (col_num > 0);
