7adaabe7b416d6bf5269220adc545171ad453238
Charles Cowart <ccowart@BigMac.local>
2019-07-15 22:09:24 -0700

    Replace print() statements w/logging

--- labcontrol/db/support_files/patches/python_patches/2.py
+++ labcontrol/db/support_files/patches/python_patches/2.py
@@ -1,16 +1,40 @@
 from labcontrol.db.sql_connection import TRN
 
+import logging
+from os import environ
+
+handler = logging.StreamHandler()
+fmt_str = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+handler.setFormatter(logging.Formatter(fmt_str))
+logger = logging.getLogger(__name__)
+logger.addHandler(handler)
+debug_levels_list = {'DEBUG': logging.DEBUG,
+                     'INFO': logging.INFO,
+                     'WARNING': logging.WARNING,
+                     'ERROR': logging.ERROR,
+                     'CRITICAL': logging.CRITICAL}
+
+if 'LABCONTROL_DEBUG_LEVEL' in environ:
+    level = environ['QIITA_CLIENT_DEBUG_LEVEL']
+    if level in debug_levels_list:
+        logger.setLevel(debug_levels_list[level])
+    else:
+         raise ValueError(
+             "%s is not a valid value for QIITA_CLIENT_DEBUG_LEVEL" % level)
+    logger.debug('logging set to %s' % level)
+else:
+    logger.setLevel(logging.CRITICAL)
+    logger.debug('logging set to CRITICAL')
+
 with TRN:
     sql = """SELECT COUNT(*) FROM information_schema.columns
              WHERE table_name = 'library_prep_shotgun_process'
              AND column_name = 'kappa_hyper_plus_kit_id';"""
+    logger.debug(sql)
     TRN.add(sql)
     result = TRN.execute_fetchflatten()
+    logger.debug(str(result))
 
-    print("BEGIN TEST")
-    print(str(result))
-    print("END TEST")
-    
     # for now, assume result will be either 0 or 1.
     # if 1, then the column 'kappa_hyper_plus_kit_id' needs
     # to be renamed to 'kapa_hyper_plus_kit_id', otherwise
@@ -18,6 +42,9 @@ with TRN:
     if result == 1:
         sql = """ALTER TABLE labcontrol.library_prep_shotgun_process RENAME
         COLUMN kappa_hyper_plus_kit_id to kapa_hyper_plus_kit_id;"""
+        logger.debug(sql)
         TRN.add(sql)
         TRN.execute()
         TRN.commit()
+    else:
+        logger.debug('labcontrol.library_prep_shotgun_process unchanged')
