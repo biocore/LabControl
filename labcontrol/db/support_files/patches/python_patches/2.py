4db0fc13610b1f490445cbaf417bcc413d10dbf6
Charles Cowart <ccowart@BigMac.local>
2019-07-16 19:01:24 -0700

    Added an overdue root logging logger
    Added a root logging.logger in the main thread of the labcontrol
    process. this should make it possible to log thread-safe messages to the
    root logger from anywhere in the code simply by importing the logging
    module. Kappa->kappa python-based patch tested.

--- labcontrol/db/support_files/patches/python_patches/2.py
+++ labcontrol/db/support_files/patches/python_patches/2.py
@@ -1,50 +1,59 @@
 from labcontrol.db.sql_connection import TRN
 
 import logging
-from os import environ
-
-handler = logging.StreamHandler()
-fmt_str = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
-handler.setFormatter(logging.Formatter(fmt_str))
-logger = logging.getLogger(__name__)
-logger.addHandler(handler)
-debug_levels_list = {'DEBUG': logging.DEBUG,
-                     'INFO': logging.INFO,
-                     'WARNING': logging.WARNING,
-                     'ERROR': logging.ERROR,
-                     'CRITICAL': logging.CRITICAL}
-
-if 'LABCONTROL_DEBUG_LEVEL' in environ:
-    level = environ['QIITA_CLIENT_DEBUG_LEVEL']
-    if level in debug_levels_list:
-        logger.setLevel(debug_levels_list[level])
-    else:
-         raise ValueError(
-             "%s is not a valid value for QIITA_CLIENT_DEBUG_LEVEL" % level)
-    logger.debug('logging set to %s' % level)
-else:
-    logger.setLevel(logging.CRITICAL)
-    logger.debug('logging set to CRITICAL')
 
 with TRN:
     sql = """SELECT COUNT(*) FROM information_schema.columns
              WHERE table_name = 'library_prep_shotgun_process'
              AND column_name = 'kappa_hyper_plus_kit_id';"""
-    logger.debug(sql)
+    logging.debug(sql)
     TRN.add(sql)
-    result = TRN.execute_fetchflatten()
-    logger.debug(str(result))
+    result = TRN.execute_fetchflatten()[0]
 
     # for now, assume result will be either 0 or 1.
     # if 1, then the column 'kappa_hyper_plus_kit_id' needs
     # to be renamed to 'kapa_hyper_plus_kit_id', otherwise
     # it has already been renamed.
     if result == 1:
+        msg = ('labcontrol.library_prep_shotgun_process contains the '
+               'column name kappa_hyper_plus_kit_id. Renaming to '
+               'kapa_hyper_plus_kit_id...')
+        logging.debug(msg)
+
         sql = """ALTER TABLE labcontrol.library_prep_shotgun_process RENAME
         COLUMN kappa_hyper_plus_kit_id to kapa_hyper_plus_kit_id;"""
-        logger.debug(sql)
         TRN.add(sql)
-        TRN.execute()
-        TRN.commit()
     else:
-        logger.debug('labcontrol.library_prep_shotgun_process unchanged')
+        msg = ('labcontrol.library_prep_shotgun_process contains the '
+               'column name kapa_hyper_plus_kit_id. Skipping...')
+        logging.debug(msg)
+
+    sql = """ALTER INDEX IF EXISTS idx_shotgun_library_prep_process_kappa
+             RENAME TO idx_shotgun_library_prep_process_kapa;"""
+
+    logging.debug(sql)
+    TRN.add(sql)
+
+    sql = """ALTER TABLE labcontrol.library_prep_shotgun_process RENAME
+             CONSTRAINT fk_shotgun_library_prep_process_kappa TO
+             fk_shotgun_library_prep_process_kapa;"""
+
+    logging.debug(sql)
+    TRN.add(sql)
+
+    sql = """UPDATE labcontrol.reagent_composition_type SET description =
+             'kapa hyper plus kit' where description =
+             'kappa hyper plus kit';"""
+    TRN.add(sql)
+
+    '''
+    TODO: Need to commit (pun intended) to having all of these actions take
+    place within a single transaction, or commit() each one in turn. The
+    problem in this case is that debug messages for all statements are printed
+    beforehand, implying that they were completed when perhaps none of them
+    were committed because any one or all of them failed due to some error.
+    Note that it will raise an exception on the first error, so the user can
+    repair the first error and then still encounter more errors.
+    '''
+    TRN.execute()
+    TRN.commit()
