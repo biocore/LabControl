5aba6dcccc502131500e2d72b304af48caf5d97f
Charles Cowart <ccowart@BigMac.local>
2019-07-16 22:26:03 -0700

    Fix patch to check all needed items before modifying.

--- labcontrol/db/support_files/patches/python_patches/2.py
+++ labcontrol/db/support_files/patches/python_patches/2.py
@@ -1,8 +1,15 @@
 from labcontrol.db.sql_connection import TRN
-
 import logging
 
+
 with TRN:
+    # although TRN has its own queue, use a local list to queue statements
+    # until prerequisite queries are completed.
+    statements = []
+
+    #
+    # rename kappa_hyper_plus_kit_id in library_prep_shotgun_process
+    #
     sql = """SELECT COUNT(*) FROM information_schema.columns
              WHERE table_name = 'library_prep_shotgun_process'
              AND column_name = 'kappa_hyper_plus_kit_id';"""
@@ -16,44 +23,68 @@ with TRN:
     # it has already been renamed.
     if result == 1:
         msg = ('labcontrol.library_prep_shotgun_process contains the '
-               'column name kappa_hyper_plus_kit_id. Renaming to '
+               'column name kappa_hyper_plus_kit_id. Will rename to '
                'kapa_hyper_plus_kit_id...')
         logging.debug(msg)
 
         sql = """ALTER TABLE labcontrol.library_prep_shotgun_process RENAME
         COLUMN kappa_hyper_plus_kit_id to kapa_hyper_plus_kit_id;"""
-        TRN.add(sql)
+        statements.append(sql)
     else:
         msg = ('labcontrol.library_prep_shotgun_process contains the '
                'column name kapa_hyper_plus_kit_id. Skipping...')
         logging.debug(msg)
 
+    #
+    # rename fk_shotgun_library_prep_process_kappa in
+    # library_prep_shotgun_process
+    #
+    sql = """SELECT COUNT(*) FROM information_schema.constraint_column_usage
+                 WHERE table_name = 'library_prep_shotgun_process' AND
+                 constraint_name = 'fk_shotgun_library_prep_process_kappa';"""
+
+    logging.debug(sql)
+    TRN.add(sql)
+    result = TRN.execute_fetchflatten()[0]
+
+    if result == 1:
+        msg = ('labcontrol.library_prep_shotgun_process contains the '
+               'constraint name fk_shotgun_library_prep_process_kappa. Will '
+               'rename to fk_shotgun_library_prep_process_kapa...')
+        logging.debug(msg)
+
+        sql = """ALTER TABLE labcontrol.library_prep_shotgun_process RENAME
+                     CONSTRAINT fk_shotgun_library_prep_process_kappa TO
+                     fk_shotgun_library_prep_process_kapa;"""
+        statements.append(sql)
+    else:
+        msg = ('labcontrol.library_prep_shotgun_process contains the '
+               'constraint name fk_shotgun_library_prep_process_kapa. '
+               'Skipping...')
+        logging.debug(msg)
+
+    #
+    # rename index idx_shotgun_library_prep_process_kappa
+    #
     sql = """ALTER INDEX IF EXISTS idx_shotgun_library_prep_process_kappa
              RENAME TO idx_shotgun_library_prep_process_kapa;"""
 
     logging.debug(sql)
-    TRN.add(sql)
+    statements.append(sql)
 
-    sql = """ALTER TABLE labcontrol.library_prep_shotgun_process RENAME
-             CONSTRAINT fk_shotgun_library_prep_process_kappa TO
-             fk_shotgun_library_prep_process_kapa;"""
-
-    logging.debug(sql)
-    TRN.add(sql)
-
+    #
+    # update description string
+    #
     sql = """UPDATE labcontrol.reagent_composition_type SET description =
              'kapa hyper plus kit' where description =
              'kappa hyper plus kit';"""
-    TRN.add(sql)
 
-    '''
-    TODO: Need to commit (pun intended) to having all of these actions take
-    place within a single transaction, or commit() each one in turn. The
-    problem in this case is that debug messages for all statements are printed
-    beforehand, implying that they were completed when perhaps none of them
-    were committed because any one or all of them failed due to some error.
-    Note that it will raise an exception on the first error, so the user can
-    repair the first error and then still encounter more errors.
-    '''
+    statements.append(sql)
+
+    # slightly inefficient, but TRN.add() does perform checking in addition to
+    # appending each SQL statement to its queue.
+    for statement in statements:
+        TRN.add(statement)
+
     TRN.execute()
     TRN.commit()
