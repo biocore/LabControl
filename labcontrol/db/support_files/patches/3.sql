3dbf1f0a21931288d6d529b8fe71bda718a2659c
Charles Cowart <ccowart@BigMac.local>
2019-07-22 17:19:06 -0700

    Add changes to the appropriate table

--- labcontrol/db/support_files/patches/3.sql
+++ labcontrol/db/support_files/patches/3.sql
@@ -8,8 +8,8 @@ CREATE TABLE labcontrol.assay_type (
 
 COMMENT ON TABLE labcontrol.assay_type IS 'List of assay types. Add future types here as they become available';
 
-ALTER TABLE labcontrol.process ADD COLUMN assay_type_id INTEGER;
-ALTER TABLE labcontrol.process ADD CONSTRAINT fk_assay_type FOREIGN KEY (assay_type_id) REFERENCES labcontrol.assay_type(assay_type_id);
+ALTER TABLE labcontrol.sequencing_process ADD COLUMN assay_type_id INTEGER;
+ALTER TABLE labcontrol.sequencing_process ADD CONSTRAINT fk_assay_type FOREIGN KEY (assay_type_id) REFERENCES labcontrol.assay_type(assay_type_id);
 
 INSERT INTO labcontrol.assay_type (description) VALUES ('Amplicon');
 INSERT INTO labcontrol.assay_type (description) VALUES ('Metagenomics');
