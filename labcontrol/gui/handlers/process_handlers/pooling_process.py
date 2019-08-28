3c0ceb67a3639c5a9b9d97cd745a88d18eb8fe6b
Charles Cowart <ccowart@BigMac.local>
2019-08-28 15:18:28 -0700

    Minor fix

--- labcontrol/gui/handlers/process_handlers/pooling_process.py
+++ labcontrol/gui/handlers/process_handlers/pooling_process.py
@@ -569,9 +569,11 @@ class ComputeLibraryPoolValuesHandler(BasePoolHandler):
         output['pool_vals'] = output['pool_vals'].tolist()
         output['pool_blanks'] = output['pool_blanks']
         # We don't need to return these values to the interface
-        output.pop('raw_vals')
-        output.pop('comp_vals')
-        output.pop('func_data')
+        # CHARLIE: These values are no longer being generated, hence
+        # they don't need to be popped.
+        # output.pop('raw_vals')
+        # output.pop('comp_vals')
+        # output.pop('func_data')
         self.write(output)
 
     def _compute_pools(self, plate_info):
@@ -600,6 +602,7 @@ class ComputeLibraryPoolValuesHandler(BasePoolHandler):
 
         # store output values
         output = {}
+        #CHARLIE
         output['pool_vals'] = pool_vals
         output['pool_blanks'] = self.comp_blanks.tolist()
         output['plate_names'] = self.plate_names.tolist()
