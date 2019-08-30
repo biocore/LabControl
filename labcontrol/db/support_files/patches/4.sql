d54279585e8a37d93b25e4c88766c031cd1be669
George Armstrong <garmstrong@colgate.edu>
2019-08-30 10:50:41 -0700

    first draft of database patch for issue 503 @charles-cowart

---
+++ labcontrol/db/support_files/patches/4.sql
@@ -0,0 +1,13 @@
+-- August 30, 2019
+-- Create table to keep track of library prep kit type
+CREATE TABLE labcontrol.shotgun_library_prep_kit_type (
+    library_prep_kit_type_id SERIAL NOT NULL,
+    description          varchar(100)  NOT NULL,
+	CONSTRAINT pk_kit PRIMARY KEY ( libraryprep_type_id )
+);
+
+-- Currently the only two options for library prep kit types.
+INSERT INTO labcontrol.shotgun_library_prep_kit_type (description) VALUES ('Kapa');
+INSERT INTO labcontrol.shotgun_library_prep_kit_type (description) VALUES ('Nextera');
+
+-- TODO 503 add column to labcontrol.library_prep_shotgun_process ?
