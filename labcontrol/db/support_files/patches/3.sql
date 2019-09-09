94ee3210db58369e4d8b469428db4423bc64268c
George Armstrong <garmstrong@colgate.edu>
2019-09-09 15:45:18 -0700

    move 4.sql into 3.sql

--- labcontrol/db/support_files/patches/3.sql
+++ labcontrol/db/support_files/patches/3.sql
@@ -79,3 +79,24 @@ CREATE TABLE labcontrol.comp_to_assay_type_map (
 INSERT INTO labcontrol.comp_to_assay_type_map (composition_type_id, assay_type_id) VALUES (6,1);
 -- 8,2 maps 'shotgun library prep' to 'Metagenomics'
 INSERT INTO labcontrol.comp_to_assay_type_map (composition_type_id, assay_type_id) VALUES (8,2);
+
+-- August 30, 2019
+-- Create table to keep track of library prep kit type
+CREATE TABLE labcontrol.library_prep_shotgun_kit_type (
+    library_prep_shotgun_kit_type_id SERIAL PRIMARY KEY,
+    description varchar NOT NULL,
+    CONSTRAINT unique_kit_type_description UNIQUE ( description )
+);
+
+-- Currently the only two options for library prep kit types.
+INSERT INTO labcontrol.library_prep_shotgun_kit_type ( description ) VALUES ('KAPA HyperPlus kit');
+INSERT INTO labcontrol.library_prep_shotgun_kit_type ( description ) VALUES ('Nextera kit');
+
+-- add a column to correspond to the kit type and default to 'Kapa' (assumes first value of SERIAL is 1)
+-- default here assumes that if nothing is specified, 1 will be taken
+ALTER TABLE labcontrol.library_prep_shotgun_process ADD COLUMN library_prep_shotgun_kit_type_id integer NOT NULL DEFAULT 1;
+-- make sure that shotgun_process kit_type is indexed on
+ALTER TABLE labcontrol.library_prep_shotgun_process
+    ADD CONSTRAINT fk_library_prep_shotgun_process_kit_type_id
+    FOREIGN KEY ( library_prep_shotgun_kit_type_id )
+    REFERENCES labcontrol.library_prep_shotgun_kit_type( library_prep_shotgun_kit_type_id );
